fe2964ca82df1537b19f6a5a9611285ddca5e610
Messias69 <52548533+Messias69@users.noreply.github.com>
2019-11-23 17:50:47 +0000

    Add files via upload

---
+++ Player_Camera.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+    //mouse (void CamControl)
+    public float RotationSpeed;
+    public Transform Target, Player;
+    private float mouseX, mouseY;
+
+
+
+    //zoom (void ViewObstructed)
+    public Collider PlayerCollider;
+    private bool Parede;
+    private float ZoomSpeed = 5f;
+
+
+
+
+
+
+
+    void Start()
+    {
+        Parede = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+
+
+
+
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -20, 30);
+
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    void ViewObstructed()
+    {
+        RaycastHit hit_wall;
+        RaycastHit hit_player;
+
+
+        PlayerCollider.Raycast(new Ray( transform.position, Target.position - transform.position), out hit_player, 5f);//creates a raycast from the camera that only focuses on the player
+
+
+
+        if (Physics.Raycast(Target.position,  -Target.forward, out hit_wall, 4))//creates a raycast from the player that goes backwards 
+        {
+            Parede = true;
+          
+        }
+        else
+        {
+            Parede = false;
+        }
+
+
+        if(Parede == true)
+        {
+            //the camera goes foward if there's a wall behind the player
+            if (Vector3.Distance(Target.position, hit_wall.point) <= 4 && Vector3.Distance(transform.position, hit_player.point) >= 2f)//care with the "2f" value, needs to be exact numbers
+                transform.Translate(Vector3.forward * ZoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //the camera goes backwards if there's nothing behind the player
+            if (Vector3.Distance(transform.position, hit_player.point) <= 4.5f)
+                transform.Translate(Vector3.back * ZoomSpeed * Time.deltaTime);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    void LateUpdate() // if bug try FixedUpdate
+    {
+        CamControl();
+        ViewObstructed();
+
+    }
+
+}
---
+++ enemy_bullet_spawn_walk.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_bullet_spawn_walk : MonoBehaviour
+{
+    private bool canShoot;
+    public GameObject bala;
+    private float time;
+    public float waitTime;
+    public Transform target;
+    public Animator anim;
+
+
+
+
+
+    private enemy_walk_movement dead;
+    private GameObject Dead;
+
+    public bool aim;
+
+
+    private float distance;
+
+    void Start()
+    {
+        waitTime = Random.Range(4,10);
+        Dead = GameObject.Find("Skeleton_walking");
+        dead = Dead.GetComponent<enemy_walk_movement>();
+    }
+
+
+
+
+   // Update is called once per frame
+    void Update()
+    {
+        
+        distance = Vector3.Distance(target.transform.position, transform.position);
+        if (dead.health > 0)
+        {
+            if (distance <=100f) {
+                transform.LookAt(target);
+                time += Time.deltaTime;
+
+                if (time > waitTime)
+                {
+                    aim = true;
+                    anim.SetBool("seen", true);
+                    Invoke("shoot", 1);
+                    time = 0;
+                }
+            }
+        }
+    }
+
+
+
+
+
+    void shoot()
+    {
+        Instantiate(bala, transform.position, transform.rotation);
+        anim.SetBool("seen", false);
+        aim = false;
+    }
+}
