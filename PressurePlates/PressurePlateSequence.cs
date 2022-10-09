d12394bc19350c2439f6ad8e71ce9f860e5195b9
JoseAntonioMoreira <52548533+JoseAntonioMoreira@users.noreply.github.com>
2022-10-09 20:15:54 +0100

    Update PressurePlateSequence.cs

--- PressurePlates/PressurePlateSequence.cs
+++ PressurePlates/PressurePlateSequence.cs
@@ -1,29 +1,31 @@
+//Script to make a combination of pressure plates
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PressurePlateSequence : MonoBehaviour
 {
-  public GameObject[] sequence;
+  public GameObject[] sequence;//Add here the combination of pressure plates that you need 
   public List<PressurePlate> slabsPressed = new List<PressurePlate>();
 
-  public void CheckSequence()
+  public void CheckSequence() //This function is called on the PressurePlate script after adding a new pressure plate to check if the combination is the right one
   {
     if (slabsPressed.Count == sequence.Length)
     {
-      if (slabsPressed[slabsPressed.Count - 1].gameObject == sequence[slabsPressed.Count - 1].gameObject)
+      if (slabsPressed[slabsPressed.Count - 1].gameObject == sequence[slabsPressed.Count - 1].gameObject) //This condiction is made when the puzzle is solved
       {
-        slabsPressed[slabsPressed.Count - 1].isSolution = true;
-        GetComponent<Animator>().SetBool("Opened", true);
+        slabsPressed[slabsPressed.Count - 1].isSolution = true; ////Instruction to give the reward of for solving the puzzle
+        GetComponent<Animator>().SetBool("Opened", true);       ////
         return;
       }
     }
     else if (slabsPressed.Count > sequence.Length)
       return;
 
-    if (slabsPressed[slabsPressed.Count - 1].gameObject != sequence[slabsPressed.Count - 1].gameObject)
-    {
-      foreach (PressurePlate pressurePlate in slabsPressed)
+    if (slabsPressed[slabsPressed.Count - 1].gameObject != sequence[slabsPressed.Count - 1].gameObject)//This condition checks if the player is solving the puzzle right
+    {                                                                                                  
+      foreach (PressurePlate pressurePlate in slabsPressed)//if the player makes a mistake the puzzle resets
       {
         pressurePlate.isSolution = false;
         pressurePlate.ResetPlates();
