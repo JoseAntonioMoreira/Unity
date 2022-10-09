fef3a43a744cd549fa936602f293db8aff8dc944
JoseAntonioMoreira <52548533+JoseAntonioMoreira@users.noreply.github.com>
2022-10-09 19:00:14 +0100

    Update PressurePlate.cs

--- PressurePlates/PressurePlate.cs
+++ PressurePlates/PressurePlate.cs
@@ -1,3 +1,5 @@
+//Add this to the pressure plate gameObject
+//References the PressurePlateSequence to checks if the puzzle has been solved
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,14 +18,14 @@ public class PressurePlate : MonoBehaviour
   }
 
   private void OnTriggerEnter(Collider other)
-  {
-    sequence.slabsPressed.Add(this);
-    sequence.CheckSequence();
-    animator.SetBool("isUp", false);
+  {                             
+    sequence.slabsPressed.Add(this);      ////On contact with the player the pressure plate will check is it's one of the solutions
+    sequence.CheckSequence();             ////If the pressure plate isn't on the solution sequence 
+    animator.SetBool("isUp", false);      ////All the presure plates will reset and the player will have to make the path again
   }
 
-  private void OnTriggerExit(Collider other)
-  {
+  private void OnTriggerExit(Collider other)//If the pressure plate isn't on the solution sequence  
+  {                                         //when the player leaves, the position will reset to it's original state
     if(!isSolution)
     ResetPlates();
   }
