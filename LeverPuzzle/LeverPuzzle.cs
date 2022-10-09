79bb73791e9b9e14bc96ac7393aa4e7cacb467ab
JoseAntonioMoreira <52548533+JoseAntonioMoreira@users.noreply.github.com>
2022-10-09 18:18:18 +0100

    Update LeverPuzzle.cs

--- LeverPuzzle/LeverPuzzle.cs
+++ LeverPuzzle/LeverPuzzle.cs
@@ -1,3 +1,7 @@
+////Lever Puzzle that turns lights ON or OFF depending on the previous state of the light
+////If all lights are on the puzzle is resolved
+////The lights use the ChangeState script
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +14,7 @@ public class LeverPuzzle : MonoBehaviour
     private bool isOn = false;
 
     [SerializeField]
-    private ChangeState[] changeStates;
+    private ChangeState[] changeStates;//Array of the lights, making possible to have as many levers and lights as needed
 
     Animator anim;
     private void Start()
@@ -21,32 +25,33 @@ public class LeverPuzzle : MonoBehaviour
 
     public void ChangeLever()
     {
-        if (gameObject.name != "Cube.027") 
-        {
-            changeStates[0].ChangeInputState();
-            changeStates[1].ChangeInputState();
-        }
-        else
-            changeStates[0].ChangeInputState();
+        if (gameObject.name != "Cube.027")          ////    this condition can be removed if all the levers will have the same behaviour
+        {                                           ////    
+            changeStates[0].ChangeInputState();     ////    
+            changeStates[1].ChangeInputState();     ////    this is changing only the first 2 lights of the array
+        }                                           ////    but a loop can be made for all the lights to switch
+        else        	                            ////    or more can be added for the needed puzzle
+            changeStates[0].ChangeInputState();     ////
+            
+            
         ChangeLeverState();
-        
         CheckIfResolved();
     }
 
-    void CheckIfResolved()
+    void CheckIfResolved()  //Looks at the array of lights and if all are ON the puzzle will play the reward
     {
-        for (int i = 0; i < changeStates.Length; i++)
-        {
-            if (!changeStates[i].isOn)
-            {
-                return;
-            }
-        }
+        for (int i = 0; i < changeStates.Length; i++)   ////
+        {                                               ////    This loop will check if all the lights are ON
+            if (!changeStates[i].isOn)                  ////    If even one of them is OFF the function will
+            {                                           ////    stop working so the reward can't be played
+                return;                                 ////
+            }                                           ////
+        }                                               ////
 
-        gate.SetBool("Opened", true);
+        gate.SetBool("Opened", true);//This part is the reward for solving the puzzle
     }
 
-    private void ChangeLeverState()
+    private void ChangeLeverState()//Animation to move the lever so the player has some feedback
     {
         isOn = !isOn;
 
