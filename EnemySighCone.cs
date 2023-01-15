db4b21410174122cf967bd52e277a4ba7ffcd1d9
JoseAntonioMoreira <52548533+JoseAntonioMoreira@users.noreply.github.com>
2023-01-15 15:01:30 +0000

    Update EnemySighCone.cs

--- EnemySighCone.cs
+++ EnemySighCone.cs
@@ -1,3 +1,4 @@
+//Simple mechanic for enemies to detect player bones.
 using UnityEngine;
 
 namespace Enemy
@@ -5,25 +6,25 @@ namespace Enemy
     public class EnemySighCone : MonoBehaviour
     {
         [SerializeField]
-        private Collider[] _playerBones;
+        private Collider[] _playerBones;//colliders of the bones you want the enemy to detect
 
         [SerializeField]
-        private float _rateIncreasePerSecond = 5f;
+        private float _rateIncreasePerSecond = 5f;//rate at wich the detection will increase
 
         [SerializeField]
-        private float _rateDecreasePerSecond = 5f;
+        private float _rateDecreasePerSecond = 5f;//rate at wich the detection will decrease
 
         [SerializeField]
-        private float _maxDetectionRate = 20f;
+        private float _maxDetectionRate = 20f; //maximum detection rate the enemy can increase
 
         [SerializeField]
-        private float _distanceInstantDetecction = 3f;
+        private float _distanceInstantDetecction = 3f; //if the distance between the player and the enemy is less than this the enemy will instant detect
 
-        private bool _onCollision = false;
+        private bool _onCollision = false;// if the player is in the sightcone
 
         private Collider _other;
 
-        public float[] _detectionRate;
+        public float[] _detectionRate; // current rate the enemy as of seeing the player
 
 
         public static bool IsDetected = false;
@@ -46,14 +47,13 @@ namespace Enemy
                 LoseDetection();
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerStay(Collider other)
         {
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player"))//On collision with the player
             {
-                if (Vector3.Distance(other.transform.position, transform.position) < _distanceInstantDetecction)
+                if (Vector3.Distance(other.transform.position, transform.position) < _distanceInstantDetecction)//if the distance is larger than the instant detection
                 {
-                    Debug.Log("OGGA");
-                    for (int i = 0; i < _playerBones.Length; i++)
+                    for (int i = 0; i < _playerBones.Length; i++)//start increasing the detection for each bone found
                     {
                         _detectionRate[i] = _maxDetectionRate + 5;
                         BonesDetected(i);
@@ -71,13 +71,13 @@ namespace Enemy
 
         private void OnTriggerExit(Collider other)
         {
-            if (other.CompareTag("Player"))
+            if (other.CompareTag("Player"))//on trigger exit change the onCollision state for decreasing the detection
             {
                 _onCollision = false;
             }
         }
 
-        private void DetectBones()
+        private void DetectBones()//search for the bones in the detected player
         {
             if (_playerBones.Length > 0)
                 for (int i = 0; i < _playerBones.Length; i++)
@@ -92,14 +92,14 @@ namespace Enemy
                 }
         }
 
-        private void BonesDetected(int i)
+        private void BonesDetected(int i)//function for increasing the detection rate
         {
-            if (_detectionRate[i] < _maxDetectionRate)
+            if (_detectionRate[i] < _maxDetectionRate)//condition to keep increasing the detection rate and if the detection rate is equal to the max detection the player is detected
                 _detectionRate[i] += _rateIncreasePerSecond * Time.deltaTime;
             else
                 IsDetected = true;
 
-            if (_detectionRate[i] >= _maxDetectionRate)
+            if (_detectionRate[i] >= _maxDetectionRate)//just some visual indicator for debuging and testing while in the editor, remove before building
             {
                 Debug.DrawRay(transform.position, _playerBones[i].transform.position - transform.position, Color.red);
             }
@@ -109,7 +109,7 @@ namespace Enemy
             }
         }
 
-        private void LoseDetection()
+        private void LoseDetection()//function to remove the detection rate if the player is no more in the sight cone
         {
             float index = 0;
             for (int i = 0; i < _detectionRate.Length; i++)
